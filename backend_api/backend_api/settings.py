1953c818bef4b1be52e55ff68be213a7d6c52724
Kai.O <92730257+KaiO277@users.noreply.github.com>
2025-03-27 20:53:37 +0700

    KaiO

--- backend_api/backend_api/settings.py
+++ backend_api/backend_api/settings.py
@@ -14,9 +14,12 @@ from pathlib import Path
 import os
 from datetime import timedelta
 from dotenv import load_dotenv
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv(dotenv_path=os.path.join(BASE_DIR, '.env'))
+
+DATABASE_URL = "postgresql://postgres:uBICrpUGznZJezpmngGxZuFNXWZIbthC@interchange.proxy.rlwy.net:25723/railway"
 
 
 # Quick-start development settings - unsuitable for production
@@ -82,15 +85,19 @@ WSGI_APPLICATION = 'backend_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': os.getenv('DB_ENGINE'),
-        'NAME':  os.getenv('DB_NAME'),
-        'USER':  os.getenv('DB_USER'),
-        'PASSWORD':  os.getenv('DB_PASSWORD'),
-        'HOST':  os.getenv('DB_HOST'),
-        'PORT':  os.getenv('DB_PORT'),
-    }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': os.getenv('DB_ENGINE'),
+#         'NAME':  os.getenv('DB_NAME'),
+#         'USER':  os.getenv('DB_USER'),
+#         'PASSWORD':  os.getenv('DB_PASSWORD'),
+#         'HOST':  os.getenv('DB_HOST'),
+#         'PORT':  os.getenv('DB_PORT'),
+#     }
+# }
+
+DATABASE = {
+    'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=1800)
 }
 
 
