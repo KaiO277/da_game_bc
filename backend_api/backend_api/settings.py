a1fc22c4312a2eddf0b51e577b65b9b206a90e5e
Kai.O <92730257+KaiO277@users.noreply.github.com>
2025-03-30 09:06:19 +0700

    KaiO

--- backend_api/backend_api/settings.py
+++ backend_api/backend_api/settings.py
@@ -151,8 +151,8 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')  
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# MEDIA_URL = '/media/' 
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  
+MEDIA_URL = '/media/' 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  
 
 CORS_ALLOW_ALL_ORIGINS = True  
 CORS_ALLOWED_ORIGINS = [
