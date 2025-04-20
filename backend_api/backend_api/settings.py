cb056365a0a5d0d92ec85796f0248a0c02dd9181
Kai.O <92730257+KaiO277@users.noreply.github.com>
2025-04-20 22:15:02 +0700

    KaiO

--- backend_api/backend_api/settings.py
+++ backend_api/backend_api/settings.py
@@ -89,20 +89,20 @@ WSGI_APPLICATION = 'backend_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': os.getenv('DB_ENGINE'),
-        'NAME':  os.getenv('DB_NAME'),
-        'USER':  os.getenv('DB_USER'),
-        'PASSWORD':  os.getenv('DB_PASSWORD'),
-        'HOST':  os.getenv('DB_HOST'),
-        'PORT':  os.getenv('DB_PORT'),
-    }
-}
-
 # DATABASES = {
-#     'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=1800)
+#     'default': {
+#         'ENGINE': os.getenv('DB_ENGINE'),
+#         'NAME':  os.getenv('DB_NAME'),
+#         'USER':  os.getenv('DB_USER'),
+#         'PASSWORD':  os.getenv('DB_PASSWORD'),
+#         'HOST':  os.getenv('DB_HOST'),
+#         'PORT':  os.getenv('DB_PORT'),
+#     }
 # }
+
+DATABASES = {
+    'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=1800)
+}
 
 
 
