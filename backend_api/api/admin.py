2c881d78f955552555848372e3eb50274e9cdf7b
Kai.O <92730257+KaiO277@users.noreply.github.com>
2025-04-27 21:22:42 +0700

    KaiO

--- backend_api/api/admin.py
+++ backend_api/api/admin.py
@@ -5,7 +5,7 @@ from .submodels.models_test import *
 
 # Register your models here.
 
-admin.site.register(User)
+# admin.site.register(User)
 admin.site.register(NFT)
 admin.site.register(Product)
 admin.site.register(Race)
