28056f17d76b49fa7d18fbce98d059e81716e9bd
Kai.O <92730257+KaiO277@users.noreply.github.com>
2025-04-27 21:17:57 +0700

    KaiO

--- backend_api/api/admin.py
+++ backend_api/api/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
 from .submodels.models_test import *
 
 
 # Register your models here.
 
 admin.site.register(User)
-# admin.site.register()
+admin.site.register(NFT)
+admin.site.register(Product)
+admin.site.register(Race)
+admin.site.register(Bet)
+admin.site.register(Transaction)
+
+
