cfa824dca748d031fd92fac114b13945b13376c9
Kai.O <92730257+KaiO277@users.noreply.github.com>
2025-05-01 09:28:53 +0700

    KaiO

--- backend_api/api/NTF/urls.py
+++ backend_api/api/NTF/urls.py
@@ -3,9 +3,6 @@ from django.urls import path
 from .views import *
 from . import views
 
-get_all_ntfs_api = NTFsMVS.as_view({
-    'get': 'get_all_ntfs_api',
-})
 
 get_ntfs_by_user_id_api = NTFsMVS.as_view({
     'get': 'get_ntfs_by_user_id_api'
@@ -24,7 +21,6 @@ update_staked_api = NTFsMVS.as_view({
 })
 
 urlpatterns = [
-    # path('', get_all_ntfs_api),
     path('get_ntfs_by_user_id_api/', get_ntfs_by_user_id_api),
     path('mint/', add_ntfs_api),
     path('update_staked_api/', update_staked_api),
