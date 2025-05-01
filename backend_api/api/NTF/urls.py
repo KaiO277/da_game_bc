4aebe5f094f70acdedff340b158b087d4a93e017
Kai.O <92730257+KaiO277@users.noreply.github.com>
2025-05-01 09:26:56 +0700

    KaiO

--- backend_api/api/NTF/urls.py
+++ backend_api/api/NTF/urls.py
@@ -24,7 +24,7 @@ update_staked_api = NTFsMVS.as_view({
 })
 
 urlpatterns = [
-    path('', get_all_ntfs_api),
+    # path('', get_all_ntfs_api),
     path('get_ntfs_by_user_id_api/', get_ntfs_by_user_id_api),
     path('mint/', add_ntfs_api),
     path('update_staked_api/', update_staked_api),
