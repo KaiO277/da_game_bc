b3408a231c9df3bca82ad4c77bbbf733babb629c
Kai.O <92730257+KaiO277@users.noreply.github.com>
2025-05-21 07:56:17 +0700

    KaiO

--- backend_api/backend_api/settings.py
+++ backend_api/backend_api/settings.py
@@ -104,8 +104,6 @@ DATABASES = {
     'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=1800)
 }
 
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
